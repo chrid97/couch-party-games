9738e6d2b69003215c39259aab041b4f1b1fa2cf
Chris <cgoncal12@gmail.com>
2020-09-21 01:39:59 -0400

    Updates

---
+++ precache-manifest.b34362228c818a1bafe542c030bf5605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bbed4284010bd17a7358822123c142",
+    "url": "/couch-party-games/index.html"
+  },
+  {
+    "revision": "dea6fffeb7eafbef2a09",
+    "url": "/couch-party-games/static/css/main.c299fff7.chunk.css"
+  },
+  {
+    "revision": "ef14f5dbaa3acea3b27a",
+    "url": "/couch-party-games/static/js/2.0de330dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/couch-party-games/static/js/2.0de330dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea6fffeb7eafbef2a09",
+    "url": "/couch-party-games/static/js/main.a5ad7a78.chunk.js"
+  },
+  {
+    "revision": "4b66222235923b581405",
+    "url": "/couch-party-games/static/js/runtime-main.e06fdcb4.js"
+  }
+]);
