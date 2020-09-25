f443d98bce84090029a957e255ea10529932413b
Chris <cgoncal12@gmail.com>
2020-09-24 23:54:58 -0400

    Seperate Header component into its own file

---
+++ src/header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import MagnifyingGlass from "./images/magnifying-glass.svg";
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header center">
+      <div className="center main">
+        <h1>
+          Discover what <span className="color">games</span> you&lsquo;ll play
+          with your friends!
+        </h1>
+        <div className="search-bar center">
+          <img src={MagnifyingGlass} alt="Magnifying Glass" />
+          <input
+            className="search"
+            type="text"
+            placeholder="Type in your favorite game to get similar games!"
+          ></input>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
