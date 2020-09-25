2ea6cb7793918c21c8eb668db29ebd10809d77ca
Chris <cgoncal12@gmail.com>
2020-09-24 20:47:26 -0400

    Add hover to game cards Add another filter bar Add api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,99 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
-import MagnifyingGlass from './images/magnifying-glass.svg';
+import MagnifyingGlass from "./images/magnifying-glass.svg";
+import axios from "axios";
+
+export interface Game {
+  id: number;
+  name: string;
+  game_modes: [];
+  cover: Cover;
+  multiplayerModes: MultiplayerModes;
+  genres: Genres;
+  keywords: string[];
+  platforms: any;
+  player_perspectives: any;
+  screenshots: Screenshot[];
+  artworks: any;
+  storyline: string;
+  summary: string;
+  tags: string[];
+  themes: Theme[];
+  url: string;
+  videos: Video[];
+  websites: string[];
+}
+
+export interface MultiplayerModes {
+  id: number;
+  campaigncoop: boolean;
+  dropin: boolean;
+  game: number;
+  lancoop: boolean;
+  offlinecoop: boolean;
+  offlinecoopmax?: number; // exists if offlinecoop is true
+  offlinemax?: number;
+  onlinecoop: boolean;
+  onlinecoopmax?: number; // exists if onlinecoop is true
+  onlinemax?: number;
+  platform?: number;
+  splitscreen: boolean;
+}
+
+export interface Genres {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+export interface Cover {
+  id: number;
+  image_id: string;
+}
+
+export interface GameMode {
+  id: number;
+}
+
+export interface Theme {
+  id: number;
+  image_id: string;
+  slug: string;
+}
+
+export interface Screenshot {
+  id: number;
+  image_id: string;
+}
+
+export interface Cover {
+  id: number;
+  image_id: string;
+}
+
+export interface Video {
+  id: number;
+  video_id: string;
+}
 
 function App() {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:4000/games").then((res) => {
+      setGames(res.data);
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div className="App center">
-      <Navbar />
+      <Navbar games={games} />
     </div>
   );
 }
 
-function Navbar() {
+function Navbar({ games }: { games: Game[] }) {
   return (
     <Router>
       <nav>
@@ -21,7 +103,7 @@ function Navbar() {
 
       <Switch>
         <Route path="/browse">
-          <Browse />
+          <Browse games={games} />
         </Route>
         <Route path="/game">
           <Game />
@@ -48,8 +130,7 @@ function Header() {
     <header className="header center">
       <div className="center main">
         <h1>
-          Discover what <span className="color">games</span> you&lsquo;ll play
-          with your friends!
+          Discover what <span className="color">games</span> you&lsquo;ll play with your friends!
         </h1>
         <div className="search-bar center">
           <img src={MagnifyingGlass} alt="Magnifying Glass" />
@@ -86,36 +167,21 @@ function Card({ title }: { title: string }) {
   );
 }
 
-function Browse() {
+function Browse({ games }: { games: Game[] }) {
   return (
     <div className="browse">
       <Filters />
-      <GameList />
+      <GameList games={games} />
     </div>
   );
 }
 
-function GameList() {
+function GameList({ games }: { games: Game[] }) {
   return (
     <div className="game-list">
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
+      {games.map((game) => (
+        <GameCard key={game.id} game={game} />
+      ))}
     </div>
   );
 }
@@ -124,6 +190,7 @@ function Filters() {
   return (
     <div className="filters">
       <FilterInput text="Type of Co-Op" />
+      <FilterInput text="# of Players" />
       <FilterInput text="Genres" />
       <FilterInput text="Themes" />
       <FilterInput text="Keywords" />
@@ -143,11 +210,17 @@ function FilterInput({ text }: { text: string }) {
   );
 }
 
-function GameCard() {
+function GameCard({ game }: { game: Game }) {
   return (
     <Link to="game" className="game-card">
-      <div className="card2"></div>
-      <p>Fall Guys</p>
+      <div
+//         style={{
+//           backgroundImage: `url(https://images.igdb.com/igdb/image/upload/t_cover_small/${game.cover.image_id}.jpg
+// ${game})`,
+//         }}
+        className="card2"
+      ></div>
+      <p>{game.name}</p>
     </Link>
   );
 }
