de5a453197b53461fcc58eb95dbc4bfba3200582
Chris <cgoncal12@gmail.com>
2020-10-15 17:32:48 -0400

    Add backend files

---
+++ backend/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const igdb string = "https://api-v3.igdb.com/games"
+const IGDB_MAX_LIMIT uint16 = 500
+
+func main() {
+	handleRequests()
+}
+
+func handleRequests() {
+	http.HandleFunc("/games", getGames)
+	http.HandleFunc("/json", serveJson)
+	log.Fatal(http.ListenAndServe(":4000", nil))
+}
+
+func getGames(w http.ResponseWriter, r *http.Request) {
+	fields := []string{"name", "slug", "game_modes.name", "cover.image_id", "multiplayer_modes.*", "genres.name", "keywords.name", "platforms.name", "player_perspectives.name", "screenshots.image_id", "artworks.image_id", "storyline", "summary", "tags", "themes.name", "videos.video_id", "websites.url"}
+
+	config, _ := readToml()
+	
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	query := fmt.Sprintf("fields %v; where %v; limit %v;", strings.Join(fields, ", "), createSlugs(config.Games), IGDB_MAX_LIMIT)
+	resBody := strings.NewReader(query)
+	client := &http.Client{
+		// CheckRedirect: redirectPolicyFunc,
+	}
+	req, err := http.NewRequest("GET", igdb, resBody)
+	req.Header.Add("user-key", config.Key)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	w.Write([]byte(body))
+}
+
+func serveJson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	body, err := ioutil.ReadFile("./games.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	w.Write([]byte(body))
+}
+
+type config struct {
+	Key string
+	Games []game
+}
+
+type game struct {
+	Slug string
+}
+
+func readToml() (*config, error) {
+	var config config
+	if _, err := toml.DecodeFile("games.toml", &config); err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	
+	return &config, nil
+}
+
+func createSlugs(games []game) string { 
+	conditions := []string{}
+	for _, game := range games {
+		slug := fmt.Sprintf(`(slug = "%v")`, game.Slug)
+		conditions = append(conditions, slug)
+	}
+
+	return strings.Join(conditions, " | ")
+}
